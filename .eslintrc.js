18983e0d3c5ef6caa502af03461a5fea497fb8ca
Michael Seele <Michael.Seele@guh-software.de>
2021-04-20 12:19:05 +0200

    feat: show issue

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+}
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-5 pl-96 pr-96 space-y-2 w-screen h-screen bg-gray-100">
+    <Select v-model="items" label="Status" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Select from './components/Select.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { Select },
+})
+</script>
