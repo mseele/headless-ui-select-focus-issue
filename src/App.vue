855204c3fa2692cdbda3a6034ed3f786f35ed394
mseele <mseele@gmail.com>
2021-04-21 08:30:10 +0200

    refactor: implement example of the issue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,178 @@
 <template>
-  <div class="p-5 pl-96 pr-96 space-y-2 w-screen h-screen bg-gray-100">
-    <Select v-model="items" label="Status" />
+  <div class="p-20 bg-gray-100 h-screen">
+    <div class="font-medium">Issue</div>
+    <div class="w-72">
+      <Listbox v-model="selectedPerson" :disabled="disabled">
+        <div class="relative mt-1">
+          <ListboxButton
+            class="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus:ring-2 focus:ring-opacity-75 focus:ring-white focus:ring-offset-blue-300 focus:ring-offset-2 focus:border-indigo-500 sm:text-sm"
+          >
+            <span class="block truncate">{{ selectedPerson.name }}</span>
+            <span
+              class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
+            >
+              <SelectorIcon class="w-5 h-5 text-gray-400" aria-hidden="true" />
+            </span>
+          </ListboxButton>
+
+          <transition
+            leave-active-class="transition duration-100 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+          >
+            <ListboxOptions
+              class="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            >
+              <ListboxOption
+                v-for="person in people"
+                v-slot="{ active, selected }"
+                :key="person"
+                :value="person"
+                as="template"
+              >
+                <li
+                  :class="[
+                    active ? 'text-blue-900 bg-blue-100' : 'text-gray-900',
+                    'cursor-default select-none relative py-2 pl-10 pr-4',
+                  ]"
+                >
+                  <span
+                    :class="[
+                      selected ? 'font-medium' : 'font-normal',
+                      'block truncate',
+                    ]"
+                    >{{ person.name }}</span
+                  >
+                  <span
+                    v-if="selected"
+                    class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                  >
+                    <CheckIcon class="w-5 h-5" aria-hidden="true" />
+                  </span>
+                </li>
+              </ListboxOption>
+            </ListboxOptions>
+          </transition>
+        </div>
+      </Listbox>
+    </div>
+    <div class="pt-10 font-medium">Workaround</div>
+    <div class="w-72">
+      <Listbox :key="disabled" v-model="selectedPerson" :disabled="disabled">
+        <div class="relative mt-1">
+          <ListboxButton
+            class="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus:ring-2 focus:ring-opacity-75 focus:ring-white focus:ring-offset-blue-300 focus:ring-offset-2 focus:border-indigo-500 sm:text-sm"
+          >
+            <span class="block truncate">{{ selectedPerson.name }}</span>
+            <span
+              class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
+            >
+              <SelectorIcon class="w-5 h-5 text-gray-400" aria-hidden="true" />
+            </span>
+          </ListboxButton>
+
+          <transition
+            leave-active-class="transition duration-100 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+          >
+            <ListboxOptions
+              class="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            >
+              <ListboxOption
+                v-for="person in people"
+                v-slot="{ active, selected }"
+                :key="person"
+                :value="person"
+                as="template"
+              >
+                <li
+                  :class="[
+                    active ? 'text-blue-900 bg-blue-100' : 'text-gray-900',
+                    'cursor-default select-none relative py-2 pl-10 pr-4',
+                  ]"
+                >
+                  <span
+                    :class="[
+                      selected ? 'font-medium' : 'font-normal',
+                      'block truncate',
+                    ]"
+                    >{{ person.name }}</span
+                  >
+                  <span
+                    v-if="selected"
+                    class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                  >
+                    <CheckIcon class="w-5 h-5" aria-hidden="true" />
+                  </span>
+                </li>
+              </ListboxOption>
+            </ListboxOptions>
+          </transition>
+        </div>
+      </Listbox>
+    </div>
+    <div class="pt-10 flex flex-row items-center">
+      <div class="font-medium pr-5">Enabled</div>
+      <Switch
+        v-model="enabled"
+        :class="enabled ? 'bg-red-900' : 'bg-gray-500'"
+        class="relative inline-flex flex-shrink-0 h-5 w-10 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        <span class="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          :class="enabled ? 'translate-x-5' : 'translate-x-0'"
+          class="pointer-events-none inline-block h-4 w-4 rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200"
+        />
+      </Switch>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import Select from './components/Select.vue'
+import { computed, defineComponent, ref } from 'vue'
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+  Switch,
+} from '@headlessui/vue'
+import { CheckIcon, SelectorIcon } from '@heroicons/vue/solid'
 
 export default defineComponent({
   name: 'App',
-  components: { Select },
+  components: {
+    Listbox,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption,
+    CheckIcon,
+    SelectorIcon,
+    Switch,
+  },
+  setup() {
+    const people = [
+      { name: 'Wade Cooper' },
+      { name: 'Arlene Mccoy' },
+      { name: 'Devon Webb' },
+      { name: 'Tom Cook' },
+      { name: 'Tanya Fox' },
+      { name: 'Hellen Schmidt' },
+    ]
+    const selectedPerson = ref(people[0])
+
+    const enabled = ref(true)
+
+    const disabled = computed(() => !enabled.value)
+
+    return {
+      people,
+      selectedPerson,
+      disabled,
+      enabled,
+    }
+  },
 })
 </script>
